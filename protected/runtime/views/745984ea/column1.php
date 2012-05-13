7bc5d1cf91880222951b5fa181526f6a58682c77
streamoso <streamoso@elmo.(none)>
2012-05-13 19:56:45 -0400

    add gitignore x

--- protected/runtime/views/745984ea/column1.php
+++
@@ -1,8 +0,0 @@
-<?php /* source file: /Users/jax/SitesMAMP/dvtv/protected/views//layouts/column1.php */ ?>
-<?php $this->beginContent('//layouts/main'); ?>
-<div class="container">
-	<div id="content">
-		<?php echo $content; ?>
-	</div><!-- content -->
-</div>
-<?php $this->endContent(); ?>
